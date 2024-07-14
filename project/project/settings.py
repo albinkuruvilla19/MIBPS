8a26e55bbad89f98438433812ffddd2170321f27
Albin <albinkuruvilla1926@gmail.com>
2024-07-14 20:58:08 +0530

    e

--- project/project/settings.py
+++ project/project/settings.py
@@ -27,6 +27,7 @@ load_dotenv(env_path)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
