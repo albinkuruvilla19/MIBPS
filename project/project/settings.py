861b45a6d82bfa9a4716822f45a1770715897f9b
Albin <albinkuruvilla1926@gmail.com>
2024-07-16 19:24:56 +0530

    xml static1

--- project/project/settings.py
+++ project/project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mibps.in', 'www.mibps.in', '127.0.0.1']
+ALLOWED_HOSTS = ['mibps.in', 'www.mibps.in']
 
 
 # Application definition
